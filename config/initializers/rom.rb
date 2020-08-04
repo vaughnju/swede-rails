4a3ecaf030fd25d4fa6fe7ba73a21fba7a30baef
Ubuntu <ubuntu@ip-172-31-34-28.us-west-2.compute.internal>
2020-08-04 22:45:20 +0000

    working on ROM configuration

--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -1,3 +1,3 @@
-#ROM::Rails::Railtie.configure do |config|
-  #config.gateways[:default] = [:sql, ENV.fetch('INTRANET_DATABASE_URL')]
-#end
+ROM::Rails::Railtie.configure do |config|
+  config.gateways[:default] = [:sql, ENV.fetch('INTRANET_DATABASE_URL')]
+end
